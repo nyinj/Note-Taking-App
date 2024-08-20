a641d45acb20670a27f5e0b19e34022db341ea2f
nyinj <nyingye25@gmail.com>
2024-08-20 16:25:27 +0600

    reached realm but not working

---
+++ app/src/main/java/com/example/notetakingapp/AddNoteActivity.java
@@ -0,0 +1,46 @@
+package com.example.notetakingapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
+
+import io.realm.kotlin.Realm;
+
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_note);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        EditText titleInput = findViewById(R.id.titleinput);
+        EditText descriptionInput = findViewById(R.id.descriptioninput);
+        MaterialButton saveBtn = findViewById(R.id.savebtn);
+
+        Realm.init(getApplicationContext());
+        Realm realm = Realm.getDefaultInstance();
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+}
